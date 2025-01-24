25455cbb7aeda26ecf12dddfad09c2ba6defb683
richJJackson <165043314+richJJackson@users.noreply.github.com>
2025-01-24 08:04:50 +0000

    shinylive failed attempt

--- test/deploy.R
+++ test/deploy.R
@@ -7,7 +7,12 @@ library(shinylive)
 ## set working directory
 setwd("~/Documents/GitHub/pscShiny")
 
+file_path <- "docs/"
+f <- list.files(file_path, include.dirs = F, full.names = T, recursive = T)
+file.remove(f)
+
 
 shinylive::export(appdir="test",destdir="docs")
 
-#httpuv::runStaticServer("docs")
+httpuv::runStaticServer("docs")
+httpuv::runStaticServer("docs/", port=8008)
